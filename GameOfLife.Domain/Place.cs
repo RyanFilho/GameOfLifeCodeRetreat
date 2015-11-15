98e1c8d48ee27927692bd2bd1275c9cc3675ddee
Ryan Sampaio Filho <ryanfilhoryanfilho@gmail.com>
2015-11-15 11:03:57 -0300

    Classe Position and Place

--- GameOfLife.Domain/Place.cs
+++ GameOfLife.Domain/Place.cs
@@ -8,5 +8,23 @@ namespace GameOfLife.Domain
 {
     class Place
     {
+        public Position Position { get; set; }
+        public Cell Cell { get; set; }
+
+        public Place(Position position, Cell cell)
+        {
+            Position = position;
+            Cell = cell;
+        }
+
+        public void LiveOrDie(int liveNeighbors)
+        {
+            if (Cell.live(liveNeighbors))
+                Cell = new LiveCell();
+            else
+                Cell = new DeadCell();
+        }
+
+
     }
 }
--- GameOfLife.Domain/World.cs
+++ GameOfLife.Domain/World.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace GameOfLife.Domain
 {
-    class Mundo
+    class World
     {
+        public List<Place> Mapa { get; set; }
+
+        public void Next()
+        {
+            foreach(var place in Mapa)
+            {
+                int liveNeighbors = CountLiveNeighbors(place);
+                place.LiveOrDie(liveNeighbors);
+            }
+        }
+
+        private int CountLiveNeighbors(Place place)
+        {
+
+            return 3;
+        }
     }
 }
